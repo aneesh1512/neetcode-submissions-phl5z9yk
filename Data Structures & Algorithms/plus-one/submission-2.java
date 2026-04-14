538e110141acfa03955c0917c7b418777d588a15
aneesh1512 <94799547+aneesh1512@users.noreply.github.com>
2026-04-15 01:31:30 +0530

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        StringBuilder sb = new StringBuilder();
+        int cr = 1;
+        for(int i = n-1; i >= 0; i--){
+            if(digits[i] == 9 && cr == 1){
+                sb.append("0");
+            }else{
+                if(cr == 1){
+                    sb.append(digits[i]+1);
+                    cr=0;
+                }else{
+                    sb.append(digits[i]);
+                }
+            }
+        }
+        if(cr == 1){
+            sb.append(1);
+        }
+        int[] arr = Arrays.stream(sb.reverse().toString().split("")).mapToInt(Integer::parseInt).toArray();
+        return arr;
+    }
+}
