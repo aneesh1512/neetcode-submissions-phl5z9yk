dde719636096406d845f8e50523df5751fed12bc
aneesh1512 <94799547+aneesh1512@users.noreply.github.com>
2026-09-02 10:14:47 +0530

    Add: longest-repeating-substring-with-replacement - submission-22

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-22.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        char arr[] = s.toCharArray();
+       Set<Character> set = s.chars()
+        .mapToObj(c -> (char) c)
+        .collect(Collectors.toSet());
+        System.out.println(set);
+        int n = s.length();
+        int ans = 0;
+        for(Character ch: set){
+            int l = 0;
+            int r = 0;
+            int temp  = k;
+            while( r < n){
+                if(s.charAt(r) != ch){
+                    if(temp == 0){
+                        while(s.charAt(l) == ch){
+                            l++;
+                        }
+                        l++;
+                        temp++;
+                        continue;
+                    }
+                    temp--;
+                }
+                    r++;
+                ans = Math.max(ans, (r-l));
+            }
+            
+            if(temp != 0)
+                ans = Math.min(Math.max(ans, (r-l+temp)), s.length());
+        }
+
+
+        return ans;
+    }
+}
